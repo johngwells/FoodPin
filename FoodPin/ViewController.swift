feaca0cdf6145f49d4003cda4f0352a8c731136c
Johnny Mandela <JW@Johnnys-MacBook-Pro.local>
2017-03-09 09:47:27 -0800

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by Johnny Mandela on 3/9/17.
+//  Copyright © 2017 Johnny Mandela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

9c7b9d7191cded1e7b79c72780b45694e5b530bd
Johnny Mandela <JW@Johnnys-MacBook-Pro.local>
2017-03-14 03:08:35 -0700

    Created a blur effect for the background in the detail view with a new class ReviewViewController

---
+++ FoodPin/ReviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by Johnny Mandela on 3/14/17.
+//  Copyright © 2017 Johnny Mandela. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

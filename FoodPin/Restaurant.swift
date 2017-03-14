281a6bf71dfaabed2dff20eddc61010881b11ea7
Johnny Mandela <JW@Johnnys-MacBook-Pro.local>
2017-03-13 18:27:25 -0700

    created a new swift file "Restaurant" class

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,25 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Johnny Mandela on 3/13/17.
+//  Copyright © 2017 Johnny Mandela. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    
+    init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isVisited = isVisited
+    }
+}

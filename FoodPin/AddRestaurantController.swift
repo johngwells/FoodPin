3c907b7d6272ab693bee03f983df3dbb35eba14c
Johnny Mandela <JW@johnnys-macbook-pro.local>
2017-04-07 01:59:19 -0700

    Completed Add New Restaurant Feature

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -13,6 +13,46 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
     
     // We need to declare an outlet var so the image can be set to the chosen image
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var typeTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var yesButton: UIButton!
+    @IBOutlet var noButton: UIButton!
+    
+    var isVisited = true
+    
+    @IBAction func save(sender: AnyObject) {
+        if nameTextField.text == "" || typeTextField.text == "" || locationTextField.text == "" {
+            let alertController = UIAlertController(title: "Oops", message: "We can't proceed because one of the fields is blank. Please not that all fields are required.",
+                preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        // Returns user to home screen
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func toggleBeenHereButton(sender: UIButton) {
+        // Ye button clicked
+        if sender == yesButton {
+            isVisited = true
+            
+            // Change the backgroundColor property of yesButton to red
+            yesButton.backgroundColor = UIColor(red: 218.0/255.0, green: 100.0/255.0, blue: 70.0/255.0, alpha: 1.0)
+            // Change the backgroundColor property of noButton to gray
+            noButton.backgroundColor = UIColor(red: 218.0/255.0, green: 223.0/255.0, blue: 225.0/255.0, alpha: 1.0)
+        } else if sender == noButton {
+            isVisited = false
+            
+            // Change the backgroundcolor property of yesButton to gray
+            yesButton.backgroundColor = UIColor(red: 218.0/255.0, green: 223.0/255.0, blue: 225.0/255.0, alpha: 1.0)
+            // Change the backgroundcolor property of noButton to red
+            noButton.backgroundColor = UIColor(red: 218.0/255.0, green: 100.0/255.0, blue: 70.0/255.0, alpha: 1.0)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
